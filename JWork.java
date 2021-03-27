2e8ca6194f58ae1cdcb3e2926f97e20cc710c51f
Haidar Hanif <45656365+whyfoo@users.noreply.github.com>
2021-03-27 09:35:21 +0700

    Modul 3 - Post Test - Revisi 1

--- JWork.java
+++ JWork.java
@@ -14,7 +14,7 @@ public class JWork
         Recruiter recruiter1 = new Recruiter(1806148694, "FM", "fm@ui.ac.id", "808080", location1);
         Job job1 = new Job(777, "UI Engineer", recruiter1, 6000000, JobCategory.UI);
         Jobseeker js1 = new Jobseeker(707070, "Pencari", "pencari@ui.ac.id", "pass", "20-03-2021");
-        Invoice inv1 = new Invoice(666, 222, "20-03-2021", 6500000, js1, PaymentType.EwalletPayment, InvoiceStatus.OnGoing);
+        Invoice inv1 = new Invoice(666, job1.getId(), "27-03-2021", job1.getFee(), js1, PaymentType.EwalletPayment, InvoiceStatus.OnGoing);
         
         //System.out.println(InvoiceStatus.Ongoing.toString());
         //System.out.println(InvoiceStatus.Finished.toString());
