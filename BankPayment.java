6087ebe66401d53f9f8730860d3dbbfe6c5b3955
Haidar Hanif <45656365+whyfoo@users.noreply.github.com>
2021-04-03 09:20:20 +0700

    Modul 4 - Post Test

---
+++ BankPayment.java
@@ -0,0 +1,91 @@
+
+/**
+ * subclass BankPayment dari invoice
+ *
+ * @author Haidar Hanif
+ * @version 3-04-2021
+ */
+public class BankPayment extends Invoice
+{
+    private static final PaymentType PAYMENT_TYPE = PaymentType.BankPayment;
+    private int adminFee;
+
+    /**
+     * Constructor for objects of class EwallletPayment
+     */
+    public BankPayment(int id, Job job, String date, Jobseeker jobseeker, InvoiceStatus invoiceStatus)
+    {
+        super(id, job, date, jobseeker, invoiceStatus);
+    }
+    
+        /**
+     * Constructor for objects of class EwallletPayment
+     */
+    public BankPayment(int id, Job job, String date, Jobseeker jobseeker, InvoiceStatus invoiceStatus, int adminFee)
+    {
+        super(id, job, date, jobseeker, invoiceStatus);
+        this.adminFee = adminFee;
+    }
+
+                /**
+     * getter untuk mengambil tipe pembayaran paymentType
+     *
+     * @return    PaymentType (Ewallet)
+     */
+    @Override
+    public PaymentType getPaymentType()
+    {
+        return PAYMENT_TYPE;
+    }
+    
+                    /**
+     * getter untuk mengambil data adminFee
+     *
+     * @return    adminFee nilai adminFee
+     */
+    public int getAdminFee()
+    {
+        return adminFee;
+    }
+    
+                /**
+     * setter untuk mengatur nilai adminFee
+     *
+     * @param  adminFee  parameter untuk mengganti adminFee
+     */
+    public void setAdminFee(int adminFee)
+    {
+        this.adminFee = adminFee;
+    }
+    
+                    /**
+     * setter untuk mengatur nilai total fee
+     *
+     */
+    @Override
+    public void setTotalFee()
+    {
+        if (adminFee > 0) {
+            totalFee = getJob().getFee() - adminFee;
+        }
+        else {
+            totalFee = getJob().getFee();
+        }
+    }
+    
+            /**
+     * metode untuk mencetak data
+     */
+    @Override
+    public void printData(){           
+        System.out.println("===================== INVOICE =====================");
+        System.out.println("ID: " + getId());
+        System.out.println("Job: " + getJob().getName());
+        System.out.println("Date: " + getDate());
+        System.out.println("Job Seeker: " + getJobseeker().getName());
+        System.out.println("Admin Fee: " + adminFee);       
+        System.out.println("Fee: " + totalFee);
+        System.out.println("Status: " + getInvoiceStatus().toString());
+        System.out.println("Payment Type: " + PAYMENT_TYPE);
+    }
+}
--- JWork.java
+++ JWork.java
@@ -12,21 +12,22 @@ public class JWork
     {
         Location location1 = new Location("Jawa Barat", "Bekasi", "Kota Patriot");
         Recruiter recruiter1 = new Recruiter(1806148694, "FM", "fm@ui.ac.id", "808080", location1);
-        Jobseeker js1 = new Jobseeker(707070, "Pencari", "pencari@ui.ac.id", "pass", "20-03-2021");
-        Bonus bonus1 = new Bonus(1, "GRATISX", 10, 60, true);
-        Bonus bonus2 = new Bonus(1, "GRATISX", 10, 40, true);
+        Jobseeker js1 = new Jobseeker(707070, "Haidarh", "haidarh@ui.ac.id", "pass", "20-03-2021");
+        Bonus bonus1 = new Bonus(1, "BONUS1", 10, 60, true);
+        Bonus bonus2 = new Bonus(1, "BONUS2", 10, 40, true);
         Job job1 = new Job(777, "UI Engineer", recruiter1, 50, JobCategory.UI);
         
-        Invoice pertama = new EwalletPayment(1, job1, "1-04-2021", js1, InvoiceStatus.OnGoing);
-        Invoice kedua = new EwalletPayment(2, job1, "1-04-2021", js1, bonus1, InvoiceStatus.OnGoing);
-        Invoice ketiga = new EwalletPayment(3, job1, "1-04-2021", js1, bonus2, InvoiceStatus.OnGoing);
+        //Invoice pertama = new EwalletPayment(1, job1, "1-04-2021", js1, InvoiceStatus.OnGoing);
+        //Invoice kedua = new EwalletPayment(2, job1, "1-04-2021", js1, bonus1, InvoiceStatus.OnGoing);
+        //Invoice ketiga = new EwalletPayment(3, job1, "1-04-2021", js1, bonus2, InvoiceStatus.OnGoing);
+        
+        Invoice pertama = new BankPayment(1, job1, "3-04-2021", js1, InvoiceStatus.Finished);
+        Invoice kedua = new BankPayment(2, job1, "3-04-2021", js1, InvoiceStatus.Finished, 15);
         
         pertama.setTotalFee();
         kedua.setTotalFee();
-        ketiga.setTotalFee();
         
         pertama.printData();
         kedua.printData();
-        ketiga.printData();
     }
 }
